67626f8813929b0d9441ad135aa29f74d9f05403
Gan161003 <149460605+Gan161003@users.noreply.github.com>
2026-05-22 17:40:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -790,8 +790,14 @@ if st.button("Fetch Reddit Data"):
     # =========================================================
 
     HEADERS = {
-        "User-Agent": "KarnatakaSocialListener/1.0"
-    }
+            "User-Agent": (
+                "Mozilla/5.0 "
+                "(Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 "
+                "(KHTML, like Gecko) "
+                "Chrome/122.0.0.0 Safari/537.36"
+            )
+        }
 
     SEARCH_URL = (
         f"https://www.reddit.com/search.json?"
